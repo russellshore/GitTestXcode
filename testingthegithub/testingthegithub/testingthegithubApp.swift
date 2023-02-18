47c45e987093c645c45214df89825d007fd8bb42
Russell Shore <66648747+russellshore@users.noreply.github.com>
2023-02-18 08:07:56 -0800

    Add files via upload

---
+++ testingthegithub/testingthegithub/testingthegithubApp.swift
@@ -0,0 +1,17 @@
+//
+//  testingthegithubApp.swift
+//  testingthegithub
+//
+//  Created by Russell Shore on 2/18/23.
+//
+
+import SwiftUI
+
+@main
+struct testingthegithubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
